c0ea3f35e1e9b3ae38f30f5f14b4d09edcd460c4
kiamco <kiamcoo@gmail.com>
2020-02-15 16:31:59 -0800

    lab 5 complete

---
+++ lab5/kimkiamcoLab5.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+from urllib.request import urlopen
+from urllib.error import URLError
+
+import re
+from datetime import date
+
+
+KEYWORDS = ["research", "climate", "evolution", "cultural",
+            "leadership", "nation", "physical", 'science', 
+            "biological", "engineering"]
+
+def get_page():
+
+    page_view =""
+
+    try:
+        to_read = urlopen('http://www.nasonline.org/')
+        if(to_read != None):
+            html = to_read.read()
+            page_view = html.decode()
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+
+    return page_view
+
+
+def parse_page(page):
+    key_count = {}
+
+    if(page != None):
+        for i in KEYWORDS:
+            regex = re.compile(i, re.IGNORECASE);
+            match = regex.findall(page)
+            key_count[i] = len(match)
+
+    return key_count
+
+if __name__ == "__main__":
+    print(f'Todays date is: {date.today()}')
+    page = get_page()
+    count_by_key = parse_page(page)
+
+    for key in count_by_key:
+        print(f'\"{key}\" apears {count_by_key[key]} times')
+
